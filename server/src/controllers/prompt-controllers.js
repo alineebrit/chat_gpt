e7873cb55cbe3037727e763f461b5847ad97ffb0
alineebrit <aline.neves@ccc.ufcg.edu.br>
2023-08-20 20:31:01 -0300

    Criando o controller

---
+++ server/src/controllers/prompt-controllers.js
@@ -0,0 +1,25 @@
+const openai = require("../config/openai")
+
+module.exports = {
+    async sendText(req, res){
+        const openaiAPI = openai.configuration()
+
+        try {
+            const response = await openaiAPI.createCompletion(
+                openai.textCompletion("me de nomes de artigos de node")
+            )
+
+            return res.status(200).json({
+                sucess: true, 
+                data: response.data.choices[0].text
+            })
+        } catch (error) {
+            return res.status(400).json({
+                sucess: false,
+                error: error.response ? error.response :
+                "there was inssue on the server"
+            })
+        }
+
+    }
+}
